bfc1a81f0d37f26d06959d35a2a40e5ddfc426dc
AlanXia0118 <2569547452@qq.com>
2019-09-17 16:48:13 -0700

    Added mode Post.

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from Insta.models import Post
+
+
 # Register your models here.
+admin.site.register(Post)
+
+
--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
-# Create your models here.
+"""
+Create your models here.
+"""
+
+class Post(models.Model):
+    # blank = True 不加title可以发出去, null = True 没有title可以发出去
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to="static/images/posts",
+        format="JPEG",
+        options={"quality": 100},
+        blank=True,
+        null=True
+    )
+
